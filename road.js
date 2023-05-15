a39acaa8627be91b0f279bcf2ad3ed708a6927cc
Becky Jeong <jeongsw0710@gmail.com>
2023-05-15 16:15:57 +0900

    automatic road

--- road.js
+++ road.js
@@ -15,14 +15,23 @@ class Road{
     draw(ctx){
         ctx.lineWidth = 5;
         ctx.strokeStyle = "white";
-        ctx.beginPath();
-        ctx.moveTo(this.left, this.top);
-        ctx.lineTo(this.left, this.bottom);
-        ctx.stroke();
+
+        for (let i = 0; i <= this.laneCount; i++){
+            const x = lerp(
+                this.left,
+                this.right,
+                i/this.laneCount
+            );
 
         ctx.beginPath();
-        ctx.moveTo(this.right, this.top);
-        ctx.lineTo(this.right, this.bottom);
+        ctx.moveTo(x, this.top);
+        ctx.lineTo(x, this.bottom);
         ctx.stroke();
+        }
+       
     }
+}
+
+function lerp(A,B,t){
+    return A+(B-A)*t;
 }
